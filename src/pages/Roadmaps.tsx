6ae5d3a49dc9c073de65f21c322e18b86dd61a2b
Zikrullo Amiri <48012861+amirymax@users.noreply.github.com>
2025-01-11 10:46:11 +0300

    Update Roadmaps.tsx

--- src/pages/Roadmaps.tsx
+++ src/pages/Roadmaps.tsx
@@ -9,11 +9,9 @@ import { Link } from "react-router-dom";
 const Roadmaps = () => {
   return (
     <div className="min-h-screen bg-background text-foreground">
-      <div className="container mx-auto px-4 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold text-gradient">
           Amiri | Главное
         </Link>
-      </div>
       <div className="min-h-screen pt-20 section-padding">
         <div className="container mx-auto">
           <h1 className="text-4xl font-bold mb-8 text-gradient">Roadmaps</h1>
